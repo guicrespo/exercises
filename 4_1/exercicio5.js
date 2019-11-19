954e640dc62a1c2dd1c44577934736ea3b3096d4
guicgs <etohlif@protonmail.com>
2019-11-19 19:52:03 -0300

    exercicio 5 angulos triangulo

---
+++ 4_1/exercicio5.js
@@ -0,0 +1,11 @@
+let ang1 = "30";
+let ang2 = 60;
+let ang3 = 90;
+
+if (ang1 == 0 || ang2 == 0 || ang3 == 0) {
+    console.log("o triângulo não pode ter ângulo igual a zero")
+} else if (ang1 + ang2 + ang3 == 180) {
+    console.log(true)
+} else if (ang1 + ang2 + ang3 !== 180) {
+    console.log(false)
+}

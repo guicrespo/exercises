5925f73c30d111be715d5b7f864074a75bb7e6ab
guicgs <etohlif@protonmail.com>
2019-11-19 18:39:08 -0300

    primeiro exercício operaçõe aritméticas

---
+++ 4_1/exercicio1.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 20;
+
+let soma = (a + b);
+console.log(soma);
+
+let subtracao = (a - b);
+console.log(subtracao);
+
+let multiplicacao = (a * b);
+console.log(multiplicacao);
+
+let modulo = (b % a);
+console.log(modulo);

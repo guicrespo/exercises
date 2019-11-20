a8458c5b19b9d2a4dd9bcf5deaa545488905e02f
guicgs <etohlif@protonmail.com>
2019-11-20 18:30:30 -0300

    exercicio 2 triângulo retânguloasterisco

---
+++ 4_2/exercicio2.js
@@ -0,0 +1,13 @@
+let n = 5
+
+for (let i = "*"; i.length <= n; i += "*") {
+    console.log(i)
+}
+
+// for (let i = 0; i < n; i++) {
+//     let num='';
+//     for (let j = 0; j <= i; j++) {
+//     num += i;
+//     }
+//     console.log(num)
+// }

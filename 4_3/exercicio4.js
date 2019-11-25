e4d2912b23515d19945a23794d44238ebde2f3a5
guicgs <etohlif@protonmail.com>
2019-11-25 14:34:23 -0300

    exercíco 4 concluído

--- 4_3/exercicio4.js
+++ 4_3/exercicio4.js
@@ -1,21 +1,13 @@
 function maiorNome(array) {
     let nome = array[0];
-    let arrayNomes = [];
-    let contador = 0;
     let tamanhoNome = array[0].length;
     for (let i = 1; i < array.length; i++) {
-        if (tamanhoNome <= array[i].length) {
-            if (tamanhoNome == array[i].length) {
-                arrayNomes[contador] = tamanhoNome;
-                contador++;
-            }
+        if (tamanhoNome < array[i].length) {
             tamanhoNome = array[i].length;
             nome = array[i];
-            arrayNomes[contador] = tamanhoNome;
-            contador++;
         }
     }
-    return arrayNomes;
+    return nome;
 }
 
 console.log(maiorNome(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana', 'Fernando']))

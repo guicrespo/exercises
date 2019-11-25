c3c979697b42ae239df29defe44b2258d80588f7
guicgs <etohlif@protonmail.com>
2019-11-25 14:31:30 -0300

    tentando retornar os dois maiores nomes

--- 4_3/exercicio4.js
+++ 4_3/exercicio4.js
@@ -1,13 +1,21 @@
 function maiorNome(array) {
     let nome = array[0];
+    let arrayNomes = [];
+    let contador = 0;
     let tamanhoNome = array[0].length;
     for (let i = 1; i < array.length; i++) {
-        if (tamanhoNome < array[i].length) {
+        if (tamanhoNome <= array[i].length) {
+            if (tamanhoNome == array[i].length) {
+                arrayNomes[contador] = tamanhoNome;
+                contador++;
+            }
             tamanhoNome = array[i].length;
             nome = array[i];
+            arrayNomes[contador] = tamanhoNome;
+            contador++;
         }
     }
-    return nome;
+    return arrayNomes;
 }
 
 console.log(maiorNome(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana', 'Fernando']))

f773d340ec45a56e17f7288579719d94ea5f5eef
guicgs <etohlif@protonmail.com>
2019-12-04 19:33:20 -0300

    formatação inicial com bootstrap e data configurada com datepickerx

---
+++ 6_2/script.js
@@ -0,0 +1,17 @@
+let inputs = document.getElementsByTagName("input");
+for(let element of inputs) {
+  element.value = null;
+}
+
+let estados = document.getElementById('estados');
+let listaEstados = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
+
+listaEstados.forEach(function(item) {
+  let option = document.createElement('option');
+  option.value = item;
+  estados.appendChild(option);
+})
+
+document.getElementById('dataInicio').DatePickerX.init({
+  format: "dd/mm/yyyy"
+});

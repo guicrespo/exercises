846d78f12b2fc8ba714345130d91a488380ba250
guicgs <etohlif@protonmail.com>
2020-01-10 17:59:56 -0300

    exercício 5 concluído

---
+++ 8_4/exercise5.js
@@ -0,0 +1,27 @@
+//Crie a classe Dog que possua o atributo name. Esse atributo deve ser configurado durante a instanciação do objeto. Crie a função bark que retorna a string composta pelo atributo name concatenada com " is barking".
+
+const assert = require('assert')
+
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+
+  bark() {
+    return `${this.name} is barking`
+  }
+}
+
+let dog1 = new Dog("Apolo")
+
+assert.equal(typeof Dog.prototype, "object")
+assert.ok(dog1 instanceof Dog)
+assert.equal(dog1.name, "Apolo")
+assert.equal(dog1.bark(), "Apolo is barking")
+
+let dog2 = new Dog("Arcos")
+
+assert.equal(typeof Dog.prototype, "object")
+assert.ok(dog2 instanceof Dog)
+assert.equal(dog2.name, "Arcos")
+assert.equal(dog2.bark(), "Arcos is barking")

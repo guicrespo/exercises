64a3499ce57df98f03dbaf2ce28a86525c680230
guicgs <etohlif@protonmail.com>
2020-01-17 14:02:55 -0300

    segunda parte do exercício 4 concluída

---
+++ 9_2/exercicio4-2.js
@@ -0,0 +1,30 @@
+const messageDelay = () => Math.floor(Math.random() * 5000)
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58
+  return Math.floor(Math.random() * maxTemperature)
+}
+
+const toFarenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32
+const temperatureInFarenheit = (temperature) => console.log(`It is currently ${toFarenheit(temperature)}ºF at Mars`)
+const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`)
+
+const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`)
+
+// definição da função sendMarsTemperature...
+const sendMarsTemperature = (callback) => {
+  const didOperationSucceed = Math.random() >= 0.6;
+  if (didOperationSucceed) {
+    const tempC = getMarsTemperature();
+    return callback(tempC)
+  } else {
+    const errorReason = 'busy';
+    return handleError(errorReason);
+  }
+}
+
+// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(temperatureInFarenheit, handleError)
+
+// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(greet, handleError)
--- 9_2/exercicio4.js
+++ 9_2/exercicio4.js
@@ -1,15 +1,17 @@
+const messageDelay = () => Math.floor(Math.random() * 5000)
+
 const getMarsTemperature = () => {
   const maxTemperature = 58
   return Math.floor(Math.random() * maxTemperature)
 }
-
-const messageDelay = () => Math.floor(Math.random() * 5000)
 
 const toFarenheit = (degreeCelsius) => (degreeCelsius * 9 / 5) + 32
 
 const temperatureInFarenheit = (temperature) => console.log(`It is currently ${toFarenheit(temperature)}ºF at Mars`)
 
 const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`)
+
+const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`)
 
 // crie a função sendMarsTemperature abaixo
 const sendMarsTemperature = (marsTemp, farenheitTemp, greetings) => {
@@ -24,12 +26,12 @@ const sendMarsTemperature = (marsTemp, farenheitTemp, greetings) => {
     greetings(tempCelsius)
   }, 5000)
   
+
 }
 
 sendMarsTemperature(getMarsTemperature, temperatureInFarenheit, greet) // imprime "Mars temperature is: 20 degree Celsius", por exemplo
 // sendMarsTemperature(temperatureInFarenheit) // imprime "It is currently 47ºF at Mars", por exemplo
 // sendMarsTemperature(greet) // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
-
 
 
 

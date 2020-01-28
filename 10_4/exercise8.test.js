fc21d75a7e1205dafee99643e220b4a6da48c7e8
guicgs <etohlif@protonmail.com>
2020-01-28 01:52:53 -0300

    exercício 8 concluído, mas sem rodar assim como o exercício 4

---
+++ 10_4/exercise8.test.js
@@ -0,0 +1,28 @@
+const fetchJoke = require('./exercise8');
+
+// até onde eu consegui chegar sozinho (não funciona):
+// it('verificar chamada da API para um resultado específico', () => {
+//   const joke = fetch(API_URL, {
+//     'id': '7h3oGtrOfxc',
+//     'joke': 'Whiteboards ... are remarkable.',
+//     'status': 200
+//   })
+//   fetchJoke(joke);
+//   return joke.then(result => {
+//     expect(result).toEqual('Whiteboards ... are remarkable.');
+//   })
+// })
+
+// GABARITO (também não funciona):
+const fetch = jest.fn(() => Promise.resolve({
+  json: () => Promise.resolve({
+    'id': '7h3oGtrOfxc',
+    'joke': 'Whiteboards ... are remarkable.',
+    'status': 200
+  })
+}));
+
+it('should fetch a joke', () => {
+  return fetchJoke()
+    .then(data => expect(data).toEqual('Whiteboards ... are remarkable.'));
+});

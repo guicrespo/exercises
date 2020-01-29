38535b177140e82e032469242bb4ddb86c49c778
guicgs <etohlif@protonmail.com>
2020-01-29 17:51:56 -0300

    exercícios 1 e 2 concluídos

--- 11_1/app-11.1/src/App.js
+++ 11_1/app-11.1/src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// const taskArray = ['levar cachorro para passear', 'fazer comida', 'limpar a casa', 'dormir'];
+// const taskArrayMap = taskArray.map(element => <li>{element}</li>);
+// function createList() {
+//   return (
+//   <ol>
+//     {taskArrayMap}
+//   </ol>
+//   );
+// }
+
+// refatorada:
+// const taskArray = ['levar cachorro para passear', 'fazer comida', 'limpar a casa', 'dormir'];
+// function createList() {
+//   return (
+//   <ol>
+//     { taskArray.map(element => <li>{element}</li>) }
+//   </ol>
+//   );
+// }
+
+// refatorada com arrow function:
 const taskArray = ['levar cachorro para passear', 'fazer comida', 'limpar a casa', 'dormir'];
-const taskArrayMap = taskArray.map(element => <li>{element}</li>);
-function createList() {
-  return (
-  <ol>
-    {taskArrayMap}
-  </ol>
-  );
-}
+const createList = () => ( <ol>{ taskArray.map(item => <li>{item}</li>) }</ol> );
 
 export default createList;

fac7374967a08606b2d60a5216e970d68a593c88
guicgs <etohlif@protonmail.com>
2020-05-27 16:07:27 -0300

    ...

--- 18_3/learn-react-hooks/src/exercise/02.js
+++ 18_3/learn-react-hooks/src/exercise/02.js
@@ -1,13 +1,27 @@
 // useEffect: persistent state
 // http://localhost:3000/isolated/exercise/02.js
 
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-function Greeting({initialName = ''}) {
+function useLocalStorageState(key, defaultValue = '') {
+  const [state, setState] = useState(() => window.localStorage.getItem(key) || defaultValue);
+
+  useEffect(() => {
+    window.localStorage.setItem(key, state);
+  }, [key, state]);
+
+  return [state, setState];
+}
+
+function Greeting() {
+  // {initialName = ''} // parâmetro da função
   // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
-
+  // const nameFromLS = window.localStorage.getItem('name') || initialName;
+  // const [name, setName] = React.useState(() => nameFromLS);
+  const [name, setName] = useLocalStorageState('name');
+  // useEffect(() => {
+  //   window.localStorage.setItem('name', name);
+  // }, [name]);
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)

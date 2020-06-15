cf9b92dc77382e171317a78ddc2085513f6c1a89
guicgs <etohlif@protonmail.com>
2020-06-15 20:01:50 -0300

    exercise 1

--- 27_2/showFileContentAndBytes.js
+++ 27_2/showFileContentAndBytes.js
@@ -1,5 +1,6 @@
-const readlineSync = require('readline-sync');
+const readline = require('readline');
 const fs = require('fs');
+const { rejects } = require('assert');
 
 // const file = readlineSync.question("Type your file name: ");
 // fs.readFile(file, 'utf-8', (err, data) => {
@@ -7,24 +8,32 @@ const fs = require('fs');
 //   console.log(`The file content is: "${data}" and has ${Buffer.byteLength(data)} bytes`);
 // });
 
-const file = readlineSync.question("Type your file name: ");
-function showFileContentAndBytes(fileName) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(fileName, (err, content) => {
-      if (err) return reject(err);
-      resolve(content);
-    });
+// const file = readlineSync.question("Type your file name: ");
+// function showFileContentAndBytes(fileName) {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(fileName, (err, content) => {
+//       if (err) return reject(err);
+//       resolve(content);
+//     });
+//   });
+// }
+
+// showFileContentAndBytes(file)
+//   .then((content) => console.log(`The file content is: "${content}" and has ${Buffer.byteLength(content)} bytes`))
+//   .catch(() => console.error('File not found'));
+
+async function showFileContentAndBytes() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question("Type your file name: ", (file) => {
+    fs.readFile(file, (err, content) => {
+      if (err) return console.error('File not found');
+      console.log(`The file content is: "${content}" and has ${Buffer.byteLength(content)} bytes`);
+    })
+    rl.close();
   });
 }
 
-showFileContentAndBytes(file)
-  .then((content) => console.log(`The file content is: "${content}" and has ${Buffer.byteLength(content)} bytes`))
-  .catch(() => console.error('File not found'));
-
-//   async function showFileContentAndBytes() {
-//     const file = await readlineSync.question("Type your file name: ");
-//     fs.readFile(file, (err, content) => {
-//       if (err) return console.error('File not found');
-//       console.log(`The file content is: "${data}" and has ${Buffer.byteLength(data)} bytes`);
-//     })
-//   }
+showFileContentAndBytes();

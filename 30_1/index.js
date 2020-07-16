7ffb87bcdf33488d9cc0a6fe32e0ed2cac9c42e5
guicgs <etohlif@protonmail.com>
2020-07-16 19:37:19 -0300

    ...

--- 30_1/index.js
+++ 30_1/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Está vivo');
+  res.status(200).send('Está vivo!');
 })
 
 app.listen(3000, () => console.log('listening to port 3000'))
